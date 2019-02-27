d10e7ed6c7c9db64c70df0931dc6f6c268899ccb
Felix O <felix.o.stack@gmail.com>
2019-02-27 10:48:59 -0800

    Delayed notification(Simplified)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { LocalNotifications } from '@ionic-native/local-notifications';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -22,6 +24,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+    LocalNotifications,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { LocalNotifications } from '@ionic-native/local-notifications';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+  private localNotifications: LocalNotifications,
+  public navCtrl: NavController
+  ) {
+  }
 
+  addNotifications() {
+    // Schedule delayed notification
+    this.localNotifications.schedule({
+       text: 'Delayed ILocalNotification',
+       trigger: {
+         at: new Date(new Date().getTime() + 3600)
+       },
+       led: 'FF0000',
+       sound: null
+    });
   }
 
 }
